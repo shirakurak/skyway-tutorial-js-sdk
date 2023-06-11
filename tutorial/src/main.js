63ce2abb431002d3540300bc97c2f04de8265381
shirakurak <surakura@hotmail.co.jp>
2023-06-11 18:11:54 +0900

    feat: token

--- tutorial/src/main.js
+++ tutorial/src/main.js
@@ -1,1 +1,44 @@
 import { nowInSec, SkyWayAuthToken, SkyWayContext, SkyWayRoom, SkyWayStreamFactory, uuidV4 } from '@skyway-sdk/room';
+
+const token = new SkyWayAuthToken({
+  jti: uuidV4(),
+  iat: nowInSec(),
+  exp: nowInSec() + 60 * 60 * 24,
+  scope: {
+    app: {
+      id: 'ここにアプリケーションIDをペーストしてください',
+      turn: true,
+      actions: ['read'],
+      channels: [
+        {
+          id: '*',
+          name: '*',
+          actions: ['write'],
+          members: [
+            {
+              id: '*',
+              name: '*',
+              actions: ['write'],
+              publication: {
+                actions: ['write'],
+              },
+              subscription: {
+                actions: ['write'],
+              },
+            },
+          ],
+          sfuBots: [
+            {
+              actions: ['write'],
+              forwardings: [
+                {
+                  actions: ['write'],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  },
+}).encode('ここにシークレットキーをペーストしてください');
