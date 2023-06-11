f32953f171f078b157a7a027af78095adcf8208c
shirakurak <surakura@hotmail.co.jp>
2023-06-11 18:30:24 +0900

    feat: end

--- tutorial/src/main.js
+++ tutorial/src/main.js
@@ -47,7 +47,64 @@ const token = new SkyWayAuthToken({
 
 (async () => {
   const localVideo = document.getElementById('local-video');
-  const { audio, video } = await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
+  const buttonArea = document.getElementById('button-area');
+  const remoteMediaArea = document.getElementById('remote-media-area');
+  const roomNameInput = document.getElementById('room-name');
+
+  const myId = document.getElementById('my-id');
+  const joinButton = document.getElementById('join');
+
+  const { audio, video } =
+    await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream();
   video.attach(localVideo);
   await localVideo.play();
+
+  joinButton.onclick = async () => {
+    if (roomNameInput.value === '') return;
+
+    const context = await SkyWayContext.Create(token);
+    const room = await SkyWayRoom.FindOrCreate(context, {
+      type: 'p2p',
+      name: roomNameInput.value,
+    });
+    const me = await room.join();
+
+    myId.textContent = me.id;
+
+    await me.publish(audio);
+    await me.publish(video);
+
+    const subscribeAndAttach = (publication) => {
+      if (publication.publisher.id === me.id) return;
+
+      const subscribeButton = document.createElement('button');
+      subscribeButton.textContent = `${publication.publisher.id}: ${publication.contentType}`;
+      buttonArea.appendChild(subscribeButton);
+
+      subscribeButton.onclick = async () => {
+        const { stream } = await me.subscribe(publication.id);
+
+        let newMedia;
+        switch (stream.track.kind) {
+          case 'video':
+            newMedia = document.createElement('video');
+            newMedia.playsInline = true;
+            newMedia.autoplay = true;
+            break;
+          case 'audio':
+            newMedia = document.createElement('audio');
+            newMedia.controls = true;
+            newMedia.autoplay = true;
+            break;
+          default:
+            return;
+        }
+        stream.attach(newMedia);
+        remoteMediaArea.appendChild(newMedia);
+      };
+    };
+
+    room.publications.forEach(subscribeAndAttach);
+    room.onStreamPublished.add((e) => subscribeAndAttach(e.publication));
+  };
 })();
